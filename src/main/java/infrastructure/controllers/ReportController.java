ddf836f4492817de7784522325c95a97df9d008e
Rayan <shmrayan@gmail.com>
2025-11-23 00:27:43 -0500

    implemented the pharmacist and assistant features

--- src/main/java/infrastructure/controllers/ReportController.java
+++ src/main/java/infrastructure/controllers/ReportController.java
@@ -2,18 +2,25 @@ package infrastructure.controllers;
 
 import application.usecases.command.GenerateDrugReportCommand;
 import application.usecases.interfaces.GenerateDrugReportUseCase;
+import domain.prescription.PrescriptionRepository;
+import domain.reporting.ReportPeriod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @Controller
 @RequestMapping("/reports")
 public class ReportController {
 
     private final GenerateDrugReportUseCase reportUseCase;
+    private final PrescriptionRepository prescriptionRepo;
 
-    public ReportController(GenerateDrugReportUseCase reportUseCase) {
+    public ReportController(GenerateDrugReportUseCase reportUseCase,
+                            PrescriptionRepository prescriptionRepo) {
         this.reportUseCase = reportUseCase;
+        this.prescriptionRepo = prescriptionRepo;
     }
 
     @GetMapping("/generate")
@@ -24,8 +31,17 @@ public class ReportController {
 
     @PostMapping("/generate")
     public String generate(@ModelAttribute GenerateDrugReportCommand command, Model model) {
+
         var report = reportUseCase.execute(command);
+        var list = prescriptionRepo.findByDINAndPeriod(
+                command.din,
+                new ReportPeriod(LocalDate.parse(command.startDate),
+                                 LocalDate.parse(command.endDate))
+        );
+
         model.addAttribute("report", report);
+        model.addAttribute("prescriptions", list);
+
         return "report_result";
     }
 }
